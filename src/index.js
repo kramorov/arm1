04e7fedbf249464e4f149fe195281565e11ab9f8
kramorov.s <@Kemerovo100500>
2023-02-24 16:41:15 +0300

    1st commit Не работают посты из файлов .md

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Blog from "./components/Blog";
+
+// const theme = createTheme();
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Blog />
   </React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
