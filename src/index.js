ce496206c6cf2430631218143cdc9a312d0f4074
kramorov <kramorovsa@gmail.com>
2023-02-26 14:25:56 +0300

    static app bar

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import ReactDOM from 'react-dom/client';
 
 import CssBaseline from '@mui/material/CssBaseline';
@@ -11,13 +11,38 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import Blog from "./components/Blog";
+import ResponsiveAppBar from "./components/AppBarResponsive";
+import ButtonAppBar from "./components/BasicAppBar";
 
+// import menuSections from './components/TopMenuSettings';
+const menuSections = [
+  { title: 'Technology', url: '#' },
+  { title: 'Design', url: '#' },
+  { title: 'Culture', url: '#' },
+  { title: 'Business', url: '#' },
+  { title: 'Politics', url: '#' },
+  { title: 'Opinion', url: '#' },
+  { title: 'Science', url: '#' },
+  { title: 'Health', url: '#' },
+  { title: 'Style', url: '#' },
+  { title: 'Travel', url: '#' },
+];
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLoginClick = () => {
+    setIsLoggedIn((isLoggedIn) => !isLoggedIn);
+    console.log(isLoggedIn)
+  };
+  const getLoginState = () => {
+    return isLoggedIn;
+  };
 // const theme = createTheme();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Blog />
+    {/*<Blog />*/}
+      <ResponsiveAppBar topNavBarTitle={'My site'} topNavBarSections={menuSections} loginHandler={handleLoginClick} loginState={getLoginState}/>
   </React.StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
